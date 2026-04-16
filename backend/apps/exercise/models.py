d87718a47a70d66e05e06cf0f374d3b7e448e963
timothy timbol <ttimbol08@gmail.com>
2026-04-16 20:32:10 +0400

    feat: added models for exercise

---
+++ backend/apps/exercise/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from apps.user.models import User
+import uuid
+
+# Create your models here.
+class Session(models.Model):
+  id = models.UUIDField(
+    primary_key=True,
+    default=uuid.uuid4,
+    editable=False
+  )
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+
+class Exercise(models.Model):
+  id = models.UUIDField(
+    primary_key=True,
+    default=uuid.uuid4,
+    editable=False
+  )
+  session = models.ForeignKey(Session, on_delete=models.CASCADE)
+  exercise = models.CharField(max_length=255)
+  reps = models.IntegerField(null=True)
+  sets = models.IntegerField(null=True)
+  length = models.IntegerField(null=True)
+  notes = models.CharField(null=True)
--- backend/main/settings.py
+++ backend/main/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apps.user',
-    'apps.auth'
+    'apps.auth',
+    'apps.exercise'
 ]
 
 REST_FRAMEWORK = {
