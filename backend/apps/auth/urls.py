9304fc6502ebafb0390c2b168cf6f4cd7d3ceca8
timothy timbol <ttimbol08@gmail.com>
2026-04-21 15:43:54 +0400

    feat: added request access token

--- backend/apps/auth/urls.py
+++ backend/apps/auth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
   path('register/', views.register),
-  path('login/', views.login)
+  path('login/', views.login),
+  path('refresh/', TokenRefreshView.as_view())
 ]
--- backend/main/settings.py
+++ backend/main/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'apps.user',
     'apps.auth',
     'apps.exercise'
@@ -59,6 +60,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ("Bearer",),
 }
 
